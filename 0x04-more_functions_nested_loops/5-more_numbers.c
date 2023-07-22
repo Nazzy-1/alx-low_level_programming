e2b87f857a225bf3e2c6ea464e625173744b28c2
Nazzy-1 <nnazirite@gmail.com>
2023-07-21 17:45:27 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,18 @@
 
 void more_numbers(void)
 {
-int i;
+int i, j;
 
-for (i = 0; i <= 9; i++)
+for (i = 0; i < 10; i++)
 {
-if (i != 2 && i != 4)
+for (j = 0; j <= 14; j++)
 {
-_putchar(i + '0');
-}
+if (j > 9)
+_putchar((j / 10) + '0');
+
+_putchar((j % 10) + '0');
 }
 
 _putchar('\n');
+}
 }

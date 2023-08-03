a4232caeaecac5bc4bc1277660c18f2dab788c5c
Nazzy-1 <nnazirite@gmail.com>
2023-08-03 15:40:54 -0700

    yeppee

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,6 @@
  *
  * Return: 0 on success, 1 on failure
  */
-
 int main(int argc, char *argv[])
 {
 int num1, num2, result;
@@ -16,12 +15,12 @@ int num1, num2, result;
 if (argc != 3)
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 * num2;
+result = num1 *num2;
 
 printf("%d\n", result);
 

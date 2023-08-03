8d4fa6bc90550051ff5553c36cbc82004608fb3d
Nazzy-1 <nnazirite@gmail.com>
2023-08-03 15:23:34 -0700

    yeppee

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ * @argc: The number of command-line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}

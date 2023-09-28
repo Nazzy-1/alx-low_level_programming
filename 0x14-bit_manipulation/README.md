62f44d5cd1267291506bf663ea2334ce9172fa32
root <nnazirite@gmail.com>
2023-09-28 15:57:46 -0700

    starting again

--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,1 +1,1 @@
-0x14. C - Bit
+bit manipulation
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int _atoi(const char *s);
-int _putchar(char c);
-int get_endianness(void);
-#endif
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+#define MAX_PATH_SIZE 1024
+#define _POSIX_C_SOURCE 200809L
+
+/* HEADERS */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+
+/* PROTOTYPES */
+void exec(const char *command);
+void prompt(void);
+void one(const char *command);
+void two(const char *command);
+char *copy(char *env);
+unsigned int binary_to_uint(const char *binary);
+
+
+#endif

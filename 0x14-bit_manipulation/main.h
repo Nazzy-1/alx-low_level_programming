13904c813e6bab916b91fc8b0b9b587e6bc574e2
root <nnazirite@gmail.com>
2023-09-28 17:00:03 -0700

    task 3

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -25,5 +25,6 @@ unsigned int binary_to_uint(const char *binary);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif

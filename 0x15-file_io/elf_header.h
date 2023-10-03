96a777fbff0e2169c6d78dc7c031abc9525f790a
root <nnazirite@gmail.com>
2023-10-02 22:58:08 -0700

    last

---
+++ 0x15-file_io/elf_header.h
@@ -0,0 +1,13 @@
+#ifndef ELF_HEADER_H
+#define ELF_HEADER_H
+
+void print_addr(char *ptr);
+void print_type(char *ptr);
+void print_osabi(char *ptr);
+void print_version(char *ptr);
+void print_data(char *ptr);
+void print_magic(char *ptr);
+void check_sys(char *ptr);
+int check_elf(char *ptr);
+
+#endif  
